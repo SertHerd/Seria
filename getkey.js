e5d01bc9a2d36e3deaad3aaaf6e92c71aa51c772
Seria <147250073+SertHerd@users.noreply.github.com>
2024-02-03 23:39:39 +0700

    Update getkey.js

--- getkey.js
+++ getkey.js
@@ -1,28 +1,11 @@
-document.addEventListener("DOMContentLoaded", async () => {
-    const resultContainer = document.getElementById("resultContainer");
-    const resultMessage = document.getElementById("resultMessage");
-    const resultLink = document.getElementById("resultLink");
+const currentUrl = window.location.href;
 
-    try {
-        const response = await fetch('http://localhost:5002/api/getkey', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ hwid: 'exampleHWID' }),
-        });
+const urlObject = new URL(currentUrl);
 
-        const data = await response.json();
+const hwid = urlObject.searchParams.get("hwid");
 
-        if (data.success) {
-            resultMessage.textContent = "Key generated successfully!";
-            resultLink.innerHTML = `<a href="${data.link_get_key}" target="_blank">Link to get key</a>`;
-            resultContainer.classList.remove("hidden");
-        } else {
-            resultMessage.textContent = `Failed to generate key. Error: ${data.error}`;
-            resultContainer.classList.remove("hidden");
-        }
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
-});
+if (hwid) {
+  console.log("hwid:", hwid);
+} else {
+  console.log("Thiếu tham số hwid");
+}
