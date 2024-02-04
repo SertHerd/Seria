2fa1e669b8b2a1d9ea7ee807d963461b56ce2eee
Seria <147250073+SertHerd@users.noreply.github.com>
2024-02-04 11:40:55 +0700

    Update getkey.js

--- getkey.js
+++ getkey.js
@@ -51,11 +51,12 @@ async function gen_link_get_key(code) {
 if (hwid) {
   console.log("hwid:", hwid);
       if (data[hwid]) {
+        console.log(`valid...`);
         const currentDateTime = moment().tz('Asia/Ho_Chi_Minh');
         const keyExpirationDateTime = moment(data[hwid].ngay_het_han, 'DD-MM-YYYY HH:mm:ss');
 
         if (currentDateTime.isBefore(keyExpirationDateTime)) {
-            res.json({ success: true, link_get_key: `${data[hwid].link_get_key}` });
+            console.log({ success: true, link_get_key: `${data[hwid].link_get_key}` });
         } else {
             try {
                 const link_get_key = await gen_link_get_key(code);
@@ -65,13 +66,14 @@ if (hwid) {
                     ngay_het_han: `${ngay_het_han}`,
                 };
                 fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2), 'utf-8');
-                res.json({ success: true, link_get_key, ngay_het_han });
+                console.log({ success: true, link_get_key, ngay_het_han });
             } catch (error) {
                 console.error('Error generating link_get_key:', error.message);
-                res.json({ success: false, error: 'Failed to generate link_get_key' });
+                console.log({ success: false, error: 'Failed to generate link_get_key' });
             }
         }
     } else {
+        console.log(`create...`);
         try {
             const link_get_key  = await gen_link_get_key(code);
             data[hwid] = {
@@ -80,10 +82,10 @@ if (hwid) {
                 ngay_het_han: `${ngay_het_han}`,
             };
             fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2), 'utf-8');
-            res.json({ success: true, link_get_key, ngay_het_han });
+            console.log({ success: true, link_get_key, ngay_het_han });
         } catch (error) {
             console.error('Error generating link_get_key:', error.message);
-            res.json({ success: false, error: 'Failed to generate link_get_key' });
+            console.log({ success: false, error: 'Failed to generate link_get_key' });
         }
     }
 } else {
